f014f0c06f64d04ddaa080db29edd47867eff950
max.kurapov <max.kurapov@zone44-wd.cs.ucalgary.ca>
2017-03-06 12:51:30 -0700

    more unit tests

--- test_module.py
+++ test_module.py
@@ -19,7 +19,7 @@ class ShapesTestMethods(unittest.TestCase):
         tri = Triangle(35,2)
         self.assertEqual(tri.getArea(), 35)
 
-    def testNormalCircleRadius(self):
+    def testNormalCircle(self):
         cir = Circle(7)
         self.assertEqual(round(cir.getArea(),2), 153.94)
 
@@ -30,6 +30,30 @@ class ShapesTestMethods(unittest.TestCase):
     def testNullParametersPar(self):
         with self.assertRaises(ValueError):
             par = Parallelogram(None, None)
+
+    def testNullParametersTri(self):
+        with self.assertRaises(ValueError):
+            tri = Triangle(None, None)
+
+    def testNullParametersCir(self):
+        with self.assertRaises(ValueError):
+            cir = Circle(None)
+
+    def testNegParameterRec(self):
+        with self.assertRaises(ValueError):
+            rec = Rectangle(-5, 0)
+
+    def testNegParametersPar(self):
+        with self.assertRaises(ValueError):
+            par = Parallelogram(0, -3)
+
+    def testNegParametersTri(self):
+        with self.assertRaises(ValueError):
+            tri = Triangle(0, -5)
+
+    def testNegParametersCir(self):
+        with self.assertRaises(ValueError):
+            cir = Circle(-5)
 
 if __name__ == '__main__':
     unittest.main()
